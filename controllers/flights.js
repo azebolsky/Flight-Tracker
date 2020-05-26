a51a8555edf73686c9eda14e25d16ff1bc3d0692
ADAM <zebolsky.adam@gmail.com>
2020-05-26 08:22:57 -0500

    fix heroku errors

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,62 +1,65 @@
-const Flight = require('../models/flight');
-const Ticket = require('../models/ticket');
+const Flight = require("../models/flight");
+const Ticket = require("../models/ticket");
 
 module.exports = {
-    new: newFlight,
-    create,
-    index,
-    show,
-    createDestination,
-    delete: deleteFlight
+  new: newFlight,
+  create,
+  //   index,
+  show,
+  createDestination,
+  delete: deleteFlight,
 };
 
 function deleteFlight(req, res) {
-    Flight.findById(req.params.id, function (err, flight) {
-        flight.remove();
-        flight.save(function (err) {
-            res.redirect('/flights');
-        })
-    })
+  Flight.findById(req.params.id, function (err, flight) {
+    console.log(`deleteFlight: ${flight}`);
+    flight.remove();
+    flight.save(function (err) {
+      res.redirect("/flights");
+    });
+  });
 }
 
 function createDestination(req, res) {
-    Flight.findById(req.params.id, function (err, flight) {
-        flight.destinations.push(req.body);
-        flight.save(function (err) {
-            res.redirect(`/flights/${flight._id}`);
-        });
+  Flight.findById(req.params.id, function (err, flight) {
+    console.log(`createDestination: ${flight}`);
+    flight.destinations.push(req.body);
+    flight.save(function (err) {
+      res.redirect(`/flights/${flight._id}`);
     });
+  });
 }
 
 function show(req, res) {
-    Flight.findById(req.params.id, function (err, flight) {
-        Ticket.find({ flight: flight._id }, function (err, tickets) {
-            res.render('flights/show', {
-                flight,
-                tickets
-            });
-        });
+  Flight.findById(req.params.id, function (err, flight) {
+    Ticket.find({ flight: flight._id }, function (err, tickets) {
+      res.render("flights/show", {
+        flight,
+        tickets,
+      });
     });
+  });
 }
 
-function index(req, res) {
-    Flight.find({}, function (err, flights) {
-        res.render('flights/index', { flights });
-    });
-}
+// function index(req, res) {
+//   Flight.find({}, function (err, flights) {
+//     console.log(`index flight: ${flights}, ${err}`);
+//     res.render("flights/index", { flights });
+//   });
+// }
 
 function create(req, res) {
-    // airline, flight number, and departure date
-    for (let key in req.body) {
-        if (req.body[key] === '') delete req.body[key];
-    }
-    const flight = new Flight(req.body);
-    flight.save(function (err) {
-        if (err) return res.render('flights/new');
-        res.redirect(`/flights/${flight._id}`);
-    });
+  // airline, flight number, and departure date
+  for (let key in req.body) {
+    if (req.body[key] === "") delete req.body[key];
+  }
+  const flight = new Flight(req.body);
+  flight.save(function (err) {
+    if (err) return res.render("flights/new");
+    res.redirect(`/flights/${flight._id}`);
+  });
 }
 
 function newFlight(req, res) {
-    res.render('flights/new');
+  res.render("flights/new");
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const flightsCtrl = require('../controllers/flights');
+const flightsCtrl = require("../controllers/flights");
 
 // GET /flights/new
 // controller action is new so '/new'
-router.get('/new', flightsCtrl.new);
-router.post('/', flightsCtrl.create);
-router.get('/', flightsCtrl.index);
-router.get('/:id', flightsCtrl.show);
-router.post('/:id/destinations', flightsCtrl.createDestination);
-router.post('/:id', flightsCtrl.delete);
+router.get("/new", flightsCtrl.new);
+router.post("/", flightsCtrl.create);
+router.get("/", flightsCtrl.index);
+router.get("/:id", flightsCtrl.show);
+router.post("/:id", flightsCtrl.delete);
 
 module.exports = router;
